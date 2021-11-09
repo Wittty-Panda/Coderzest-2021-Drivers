b5abd9c754139d7cd7a81941b19388a2c45b4305
Bhavesh Joshi <bhaveshjoshi2512@gmail.com>
2021-11-09 16:22:06 +0530

    Create W4_Q1_Driver_cpp.cpp

---
+++ W4_Q1_Driver_cpp.cpp
@@ -0,0 +1,28 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}  // } Driver Code Ends
